11f796e9654d5c949f99b352429484137d643b14
Ruslan Filistovich <ruslan.filistovich@intexsoft.by>
2020-02-21 09:17:21 +0300

    Initial Commit

---
+++ MeetRoomINTXSFT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeetRoomINTXSFT
+//
+//  Created by Ruslan Filistovich on 21/02/2020.
+//  Copyright © 2020 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
